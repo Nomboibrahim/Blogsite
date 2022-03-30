c7980a979c2c1700466b165435f41d5264b07de1
nomboibrahim <nomboibrahim87@gmail.com>
2022-03-30 07:24:09 -0700

    added ruote

---
+++ src/pages/About.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function About() {
+  return (
+    <div>
+    <h1>Welcom to about me</h1>
+    <div>
+    <p>This global website presents OFFICIAL weather observations, weather forecasts
+     and climatological information for selected cities supplied by National 
+     Meteorological & Hydrological Services (NMHSs) worldwide.The NMHSs make official
+      weather observations in their respective countries. Links to their official 
+      weather service websites and tourism board/organization are also provided
+       whenever available. Weather icons are shown alongside worded forecasts in this 
+       version to facilitate visual inspection.
+
+    </p>
+    <p>This global website presents OFFICIAL weather observations, weather forecasts
+    and climatological information for selected cities supplied by National 
+    Meteorological & Hydrological Services (NMHSs) worldwide.The NMHSs make official
+     weather observations in their respective countries. Links to their official 
+     weather service websites and tourism board/organization are also provided
+      whenever available. Weather icons are shown alongside worded forecasts in this 
+      version to facilitate visual inspection.
+
+   </p>
+   <p>This global website presents OFFICIAL weather observations, weather forecasts
+   and climatological information for selected cities supplied by National 
+   Meteorological & Hydrological Services (NMHSs) worldwide.The NMHSs make official
+    weather observations in their respective countries. Links to their official 
+    weather service websites and tourism board/organization are also provided
+     whenever available. Weather icons are shown alongside worded forecasts in this 
+     version to facilitate visual inspection.
+  </p>
+    </div>
+
+    </div>
+  )
+}
+
+export default withLayout(About)
